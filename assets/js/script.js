ae5439d34e0f5337d16737f840f036dfd6c265ed
Craig5117 <craig.bennett5117@gmail.com>
2020-10-17 17:43:34 -0400

    controlled pass length and connected with HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,74 @@
 // Assignment code here
-// CharPool variables
+
+
 var lowerCh = "abcdefghijklmnopqrstuvwxyz"
 var upperCh = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var numCh = "1234567890"
 var specCh = " !”#$%&’()*+,-./:;<=>?@[\]^_`{|}~"
 var characters = ""
 
+
 var generatePassword = function(){
-  // conditionals to control CharPool array
-  var setCharPool = function() { 
-    var confirmLower = confirm("Would you like to use lowercase characters?") 
-        if (confirmLower) {
-          characters = characters + lowerCh;
-        };
-    var confirmUpper = confirm("Would you like to use uppercase characters?") 
-        if (confirmUpper) {
-        characters = characters + upperCh;
-        };
-    var confirmNum = confirm("Would you like to use numeric characters?") 
-        if (confirmNum) {
-        characters = characters + numCh;
-        };
-    var confirmSpec = confirm("Would you like to use special characters (* % / + etc.)?") 
-        if (confirmSpec) {
-        characters = characters + specCh;
-        };    
-    if (characters === "" || characters === null) {
-      alert("You must choose at least 1 character option. Please try again.")
-      setCharPool();
-    }
-  };
+    // conditionals to control array content
+    var setPassCriteria = function() { 
+      var confirmLower = confirm("Would you like to use lowercase characters?") 
+          if (confirmLower) {
+            characters = characters + lowerCh;
+          };
+      var confirmUpper = confirm("Would you like to use uppercase characters?") 
+          if (confirmUpper) {
+          characters = characters + upperCh;
+          };
+      var confirmNum = confirm("Would you like to use numeric characters?") 
+          if (confirmNum) {
+          characters = characters + numCh;
+          };
+      var confirmSpec = confirm("Would you like to use special characters (* % / + etc.)?") 
+          if (confirmSpec) {
+          characters = characters + specCh;
+          };    
+      if (characters === "" || characters === null) {
+        alert("You must choose at least 1 character option. Please try again.")
+        setPassCriteria();
+      }
+    };
 
-  setCharPool();
-
-  // converts string to array  
-  var passArray = characters.split(""); 
-  console.log(`CharPool: ${characters}`);
-  // initializes password var
-  let pass = ""
-      
-  
-
-  // assembles password
-  for (i = 0; i < passLength; i++) {
-    var randomCh = passArray[Math.floor(Math.random() * passArray.length)];
-        pass = pass + randomCh;
+    setPassCriteria();
+    // converts string to array  
+    var passArray = characters.split(""); 
+    console.log(`CharPool: ${characters}`);
+    
+    // initializes password var
+    let pass = ""
+    
+    // Controls password length
+    var setPassLength = function() {
+    let passLength = prompt("How long would you like your password to be? Please enter a number between 8 and 128.")
+        passLength = parseInt(passLength);
+        if (passLength < 8 || passLength > 128) {
+          alert("Invalid input.");
+          setPassLength();
+        };
+        return passLength
         
-  }
-console.log(`The password is (drumroll...): ${pass}`)
-return pass;
+    };
+    // sets password length to user input
+    let passLength = setPassLength();
+
+    // assembles password
+    for (i = 0; i < passLength; i++) {
+      var randomCh = passArray[Math.floor(Math.random() * passArray.length)];
+          pass = pass + randomCh;
+          
+    }
+    console.log(`The password is (drumroll...): ${pass}`)
+    return pass;
 };
+
+
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
