69e1f571c5cf854e6d42043fa7622f17b8179507
Craig5117 <craig.bennett5117@gmail.com>
2020-10-17 17:35:16 -0400

    controlled user input for CharPool array

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ var characters = ""
 
 var generatePassword = function(){
   // conditionals to control CharPool array
-  
+  var setCharPool = function() { 
     var confirmLower = confirm("Would you like to use lowercase characters?") 
         if (confirmLower) {
           characters = characters + lowerCh;
@@ -25,7 +25,13 @@ var generatePassword = function(){
         if (confirmSpec) {
         characters = characters + specCh;
         };    
-   
+    if (characters === "" || characters === null) {
+      alert("You must choose at least 1 character option. Please try again.")
+      setCharPool();
+    }
+  };
+
+  setCharPool();
 }
   // converts string to array  
   var passArray = characters.split(""); 
