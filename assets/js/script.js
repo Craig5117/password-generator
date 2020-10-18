51d661b8be83281dd7a45872a944e94b201e9e64
Craig5117 <craig.bennett5117@gmail.com>
2020-10-18 12:11:50 -0400

    killed the final bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,83 +5,91 @@ var lowerCh = "abcdefghijklmnopqrstuvwxyz"
 var upperCh = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var numCh = "1234567890"
 var specCh = " !”#$%&’()*+,-./:;<=>?@[\]^_`{|}~"
-var characters = ""
+// var characters = ""
+let characters
 
 
-// resets the CharPool
-var reset = function(){
-  characters = "" 
-  return characters
-}
+// // resets the CharPool
+// var reset = function(){
+//   characters = "" 
+//   return characters
+// }
 
 var generatePassword = function(){
-    reset();
+    // reset();
+    //bug may be here
     // conditionals to control array content
     var setCharPool = function() { 
+      characters = ""
       var confirmLower = confirm("Would you like to use lowercase characters?") 
           if (confirmLower) {
             characters = characters + lowerCh;
           };
       var confirmUpper = confirm("Would you like to use uppercase characters?") 
           if (confirmUpper) {
-          characters = characters + upperCh;
+            characters = characters + upperCh;
           };
       var confirmNum = confirm("Would you like to use numeric characters?") 
           if (confirmNum) {
-          characters = characters + numCh;
+            characters = characters + numCh;
           };
       var confirmSpec = confirm("Would you like to use special characters (* % / + etc.)?") 
           if (confirmSpec) {
-          characters = characters + specCh;
+            characters = characters + specCh;
           };    
       if (characters === "" || characters === null) {
         alert("You must choose at least 1 character option. Please try again.")
         setCharPool();
       }
+      else {
+        return characters
+      }
     };
 
     setCharPool();
     // converts string to array  
-    var passArray = characters.split(""); 
+    // var passArray = characters.split(""); 
     console.log(`CharPool: ${characters}`);
-    
+   
     // initializes password var
     let pass = ""
     
+    //Bug may be here
     // Controls password length
-    var setPassLength = function() {
-    let passLength = prompt("How long would you like your password to be? Please enter a number between 8 and 128.")
-        if (passLength === ""){
-          alert("Invalid input.");
-          setPassLength();
-        }
+    var setPass = function() {
+    let passLength = 0;
+    passLength = prompt("How long would you like your password to be? Please enter a number between 8 and 128.");
+        console.log("passLength ", passLength);
+        parseInt(passLength);
+        console.log("parsed passLength ", parseInt(passLength));
         if (passLength === null){
-          // If they cancel prompt do they really want to quit?
-          var confirmCancel = confirm("Do you want to quit or start over?")
-            if (confirmCancel) {
-              reset();
-            }
-            else {
-              setPassLength();
-            }
-        }
-        passLength = parseInt(passLength);
-        if (passLength < 8 || passLength > 128) {
+          alert("Have a nice day!");
+        } else if (isNaN(passLength) || passLength < 8 || passLength > 128 || passLength === ""){
           alert("Invalid input.");
-          setPassLength();
-        };
-        return passLength
+          passLength = 0;
+          setPass();
+        } 
+        
+        // passLength = parseInt(passLength);
+        // if (passLength < 8 || passLength > 128) {
+        //   alert("Invalid input.");
+        //   setPass();
+        // // return has to go outside the conditionals here or it doesn't work        
+        // };
+        for (i = 0; i < passLength; i++) {
+          var randomCh = characters[Math.floor(Math.random() * characters.length)];
+              pass = pass + randomCh;
+              
+        }
+        return pass
         
     };
     // sets password length to user input
-    let passLength = setPassLength();
+    // let passLength = setPassLength();
+    setPass();
 
-    // assembles password
-    for (i = 0; i < passLength; i++) {
-      var randomCh = passArray[Math.floor(Math.random() * passArray.length)];
-          pass = pass + randomCh;
-          
-    }
+    // assembles password (originally had passArray in place of characters)
+    
     
     
     console.log(`The password is (drumroll...): ${pass}`)
